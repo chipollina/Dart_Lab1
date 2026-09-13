9ad37a619a1d21021ccddfc6a0da572468217aa4
chipollina <poli345678@mail.ru>
2026-09-13 20:28:54 +0300

    learn dart functions

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -40,4 +40,38 @@ void main(){
   for (var fruit in fruits2) {
     print(fruit);
   }
+  print(greet("Артем"));
+  print(greet("Мария"));
+  print(square(5));
+  print(half(6));
+  describePet(name: 'Барсик', age: 3);
+  describePet(name: 'Шарик', species: "пес");
+  print(repeat("ха"));
+  print(repeat("ха", 3));
+  List<int> numbers = [3, 1, 4, 1, 5,9];
+  numbers.sort((a,b)=> b-a);
+  print(numbers);
+  List<String> names=["Артем","Мария", "Иван"];
+  List<String> upper = names.map  ((name)=> name.toUpperCase()).toList  ();
+  print(upper);
+  List<String> longNames = names.where  ((name)=> name.length >4).toList();
+  print(longNames);
 }
+String greet(String name){
+  return "Привет, $name!";
+}
+int square(int x) => x*x;
+double half(double x) => x/2;
+void describePet({required String name, String species = "кот", int age =0}) {
+  print("$name - $species, возраст $age");
+  
+}
+String repeat(String text, [int times=2]) {
+  String result = "";
+  for (int i=0; i<times; i++){
+    result += text;
+  }
+  return result;
+}
+
+
