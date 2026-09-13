4c6d72277e7ca910e5f12913e4990d7acaf62cfb
chipollina <poli345678@mail.ru>
2026-09-13 19:55:15 +0300

    learn dart syntax and types

---
+++ bin/lab1_dart.dart
@@ -0,0 +1,43 @@
+import 'package:lab1_dart/lab1_dart.dart' as lab1_dart;
+
+void main(){
+  String name="артем"; 
+  int age=20;
+  double height=1.75;
+  bool isStudent=true;
+  
+
+  print("Привет, $name! Тебе $age лет.");
+  print("Через 5 лет тебе будет ${age+5} лет.");
+  print("Рост: ${height} м, студент: $isStudent");
+  var score = 95;
+  var language = "Dart";
+  print("$language: $score");
+  const String appName="Lab1";
+  final int startYear=2026;
+  print("$appName started in $startYear");
+  String? city=null;
+  if (city != null) {
+    print(
+      city.toUpperCase()
+  );
+  }
+  String? nickname = null;
+  String display = 
+    nickname ?? "Аноним";
+  print(display);
+  List<String> fruits = ["яблоко", "банан", "груша"];
+  fruits.add("апельсин");
+  print(fruits[0]);
+  print(fruits.length);
+  Map<String, dynamic> person = {"name": "Артем", "age":20};
+  print(person["name"]);
+  person["city"]="Волжский";
+  Set<int> ids = {1,2,3,2,1};
+  print(ids);
+  print(ids.length);
+  List<String> fruits2=["яблоко", "банан", "груша"];
+  for (var fruit in fruits2) {
+    print(fruit);
+  }
+}
